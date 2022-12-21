7d51bc87b85209a1939ef9e887008d4346dc912d
dhensle <dhensle@users.noreply.github.com>
2022-12-21 21:08:07 +0000

    deploy: c474a2656ac50f9e279de136c21983baf40e8024

--- BayDAG/_static/documentation_options.js
+++ BayDAG/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.6.dev996+g9506f166',
+    VERSION: '0.9.6.dev997+gc474a265',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
