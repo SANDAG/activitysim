7ba0ddda7fd69a95755ed492b307315d7d2f08db
David Hensle <davidh@sandag.org>
2023-03-02 14:29:44 -0800

    blacken

--- activitysim/abm/models/cdap.py
+++ activitysim/abm/models/cdap.py
@@ -190,7 +190,9 @@ def cdap_simulate(persons_merged, persons, households, chunk_size, trace_hh_id):
         estimator.write_choices(choices)
         choices = estimator.get_survey_values(choices, "persons", "cdap_activity")
         if add_joint_tour_utility:
-            hh_joint = estimator.get_survey_values(hh_joint, "households", "has_joint_tour")
+            hh_joint = estimator.get_survey_values(
+                hh_joint, "households", "has_joint_tour"
+            )
         estimator.write_override_choices(choices)
         estimator.end_estimation()
 
